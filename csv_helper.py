77f352cacc6c1ef2694ee3006e7d67d74f40c6dc
Lucas Malinski <mailpatriota@gmail.com>
2026-04-20 18:25:49 -0300

    fix: add csv name to verbose printing

--- csv_helper.py
+++ csv_helper.py
@@ -4,6 +4,5 @@ import pandas as pd
 def load_csv(f, verbose=False):
     f_encoding = from_path(f).best().encoding
     if verbose:
-        print (f"Encoding picked for csv: {f_encoding}")
+        print (f"Encoding picked for csv {f}:\n {f_encoding}")
     return pd.read_csv(f, encoding=f_encoding, sep=";")
-    
