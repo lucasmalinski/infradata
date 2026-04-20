1addc17b92a2ad95a14cfcd8f26ea28d4a9b4963
Lucas Malinski <mailpatriota@gmail.com>
2026-04-20 18:23:46 -0300

    feat: add --verbose flag

--- csv_helper.py
+++ csv_helper.py
@@ -1,8 +1,9 @@
 from charset_normalizer import from_path
 import pandas as pd 
 
-def load_csv(f):
+def load_csv(f, verbose=False):
     f_encoding = from_path(f).best().encoding
+    if verbose:
+        print (f"Encoding picked for csv: {f_encoding}")
     return pd.read_csv(f, encoding=f_encoding, sep=";")
-
-cbox
+    
