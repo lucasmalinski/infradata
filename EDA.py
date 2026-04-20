bae57754c02e6f168622dbb2fd87d5c45c5386e4
Lucas Malinski <mailpatriota@gmail.com>
2026-04-20 18:56:28 -0300

    fix: csv/df treatments

--- EDA.py
+++ EDA.py
@@ -3,11 +3,18 @@
 from csv_helper import load_csv
 from pathlib import Path  
 
-MAIN_DIR =  Path(__file__).resolve().parent
-FROTA_PATH = MAIN_DIR / "raw_data" / "3.frota-de-veiculos-do-df-nos-ultimos-10-anos.csv"
+MAIN_DATA_DIR =  Path(__file__).resolve().parent / "raw_data"
+FROTA_PATH = MAIN_DATA_DIR / "3.frota-de-veiculos-do-df-nos-ultimos-10-anos.csv"
+HABILITADOS_PATH = MAIN_DATA_DIR / "11.numero-de-habilitados-no-distrito-federal-nos-ultimos-10-anos.csv"
+INDICE_MORTOS_PATH = MAIN_DATA_DIR /  "6.indice-mortos-por-10-mil-veiculos-no-df-nos-ultimos-10-anos.csv"
 
 
 frota = load_csv(FROTA_PATH)
+habilitados = load_csv(HABILITADOS_PATH)
+mortos_por_10k = load_csv(INDICE_MORTOS_PATH)
 
-frota
 
+habilitados = habilitados.transpose()
+habilitados.columns = ["ano", "total", "permissionario_pd", "condutor_definitivo_cnh"]
+print(habilitados)
+# %%
