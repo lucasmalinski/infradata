d2cd2c596345297e683979751289306f3e7d3163
Lucas Malinski <mailpatriota@gmail.com>
2026-04-23 21:12:01 -0300

    fix: move data transformations into processing functions

--- src/processing/frota.py
+++ src/processing/frota.py
@@ -1,14 +1,14 @@
-# %%
+from src.utils.csv_utils import load_csv
+from src.utils.df_utils import remove_thousands_separator
+import pandas as pd
+
 # =======================================
 # Tratamento 3.Frota
 # =======================================
-frota = load_csv(FROTA_PATH) 
-frota = frota.set_index("ANO")
-frota = frota.astype(str)
 
-frota = frota.apply(
-    lambda col: col.str.replace('.', '', regex=False)
-    )
-
-frota = frota.astype(int)
-print(frota)
+def process(path: Path) -> pd.DataFrame:
+    frota = load_csv(path) 
+    frota = frota.set_index("ANO")
+    frota = remove_thousands_separator(frota)
+    frota = frota.astype(int)
+    return frota
