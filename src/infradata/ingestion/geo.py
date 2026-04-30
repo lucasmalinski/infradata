69adce1fac748e5dc19a06b7382b8eb13d0cbd4b
Lucas Malinski <mailpatriota@gmail.com>
2026-04-30 16:51:04 -0300

    chore: function types and better path handling

---
+++ src/infradata/ingestion/geo.py
@@ -0,0 +1,24 @@
+import json
+from pathlib import Path
+import requests
+import geopandas as gpd
+from infradata.utils.project_root import find_project_root
+
+
+def ingest(URL: str, root: Path) -> gpd.GeoDataFrame:
+    cache = Path(root / "data" / "external" / "rodovias_cache.geojson")
+    if cache.exists():
+        print("[geo.py] Loading from cache...")
+        with open(cache, "r", encoding="utf-8") as f:
+            data = json.load(f)
+    else:
+        print("[geo.py] Fetching from API (this may take a moment)...")
+        r = requests.get(URL)
+        r.raise_for_status() 
+        data = r.json()
+
+        with open(cache, "w", encoding="utf-8") as f:
+            json.dump(data, f)
+
+    gdf = gpd.GeoDataFrame.from_features(data["features"], crs="EPSG:4326")
+    return gdf
