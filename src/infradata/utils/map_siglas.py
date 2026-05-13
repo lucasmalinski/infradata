1d4995a5dd5e639b5a5418b71b1ded830c81d1f0
Lucas Malinski <mailpatriota@gmail.com>
2026-05-13 11:41:39 -0300

    feat: add estrada parque sigla_map for data transformation

---
+++ src/infradata/utils/map_siglas.py
@@ -0,0 +1,9 @@
+sigla_map = {
+    'EPNA': 'DF-004', 'EPPR': 'DF-005', 'EPCA': 'DF-006', 'EPTT': 'DF-007',
+    'EPIG': 'DF-011', 'EPDB': 'DF-025', 'EPJK': 'DF-027', 'EPCV': 'DF-035',
+    'EPAR': 'DF-047', 'EPGU': 'DF-051', 'EPVB': 'DF-055', 'EPIP': 'DF-065',
+    'EPNB': 'DF-075', 'EPVP': 'DF-079', 'EPIB': 'DF-081', 'EPTG': 'DF-085',
+    'EPVL': 'DF-087', 'EPCL': 'DF-095', 'EPAC': 'DF-097', 'EPTM': 'DF-015',
+    'EPIA': 'DF-003', 'EPCT': 'DF-001', 'EPAA': 'DF-010', 'EPPN': 'DF-009',
+    'EPUB': 'DF-008',
+}
